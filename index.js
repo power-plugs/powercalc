149d5e51d161ffc7329d9a381f29ca5284230cf7
Lil Sizzurp <45602120+LilSizzurp@users.noreply.github.com>
2019-11-19 11:21:25 +0100

    Can now do more "advanced" maths.
    No longer num1 op num2
    You can now num1 op num2 op num3 op num4 op num5 etc..

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ module.exports = class Calc extends Plugin {
   }
 
   calc(args) {
-    if(args[0] && args[1] && args[2]) {
+    /*if(args[0] && args[1] && args[2]) {
       var num1 = Number(args[0]);
       var op = args[1];
       var num2 = Number(args[2]);
@@ -28,6 +28,117 @@ module.exports = class Calc extends Plugin {
       }
     } else {
       return `Usage: ${powercord.api.commands.prefix} calc <num1> <op> <num2>`;
+    }*/
+
+    var state = "none";
+    var addsub = [];
+    var prevVal = 0;
+    var currentval = "";
+    var fullVal = "";
+    var sum = 0;
+    var math = args.join(" ");
+      
+    function gatherdata(){
+      console.log("currentval = " + currentval);
+      console.log("prevVal = " + prevVal);
+      console.log("sum = " + sum);
     }
+
+    function updateMode(input){
+      if(state != "none") {
+        switch (state){
+          case "-":
+            addsub.push(prevVal);
+            prevVal = 0 -(parseInt(currentval));
+            currentval = "";
+            state = input;
+            break;
+            
+          case "+":
+            addsub.push(prevVal);
+            prevVal = parseInt(currentval);
+            currentval = "";
+            state = input;
+            break;
+            
+          case "*":
+            prevVal = prevVal * parseInt(currentval);
+            currentval = "";
+            state = input;
+            break;
+            
+          case "/":
+            prevVal = prevVal / parseInt(currentval);
+            currentval = "";
+            state = input;
+            break;
+          
+          case "^":
+            prevVal = Math.pow(prevVal, parseInt(currentval));
+            currentval = "";
+            state = input;
+            break;
+        }
+      } else if (input != "=") {
+        prevVal = parseInt(currentval);
+        currentval = "";
+        sum = 0;
+      
+        state = input;
+      }
+      if (input == "="){
+        addsub.push(prevVal);
+        for(var i = 0; i < addsub.length; i++){
+          sum += addsub[i];
+        }
+      }
+    }
+
+    for (let i = 0; i < math.length; i++) {
+      const e = math.charAt(i);
+      switch(e) {
+        case "+":
+            if(currentval == "") return
+            fullVal += currentval + " + ";
+            updateMode("+");
+            currentval = "";
+            break;
+
+        case "-":
+            if(currentval == "") return;
+            fullVal += currentval + " - ";
+            updateMode("-");
+            break;
+
+        case "*":
+            if(currentval == "") return;
+            fullVal += currentval + " × ";
+            updateMode("*");
+            break;
+
+        case "/":
+            if(currentval == "") return;
+            fullVal += currentval + " / ";
+            updateMode("/");
+            break;
+            
+        case "^":
+            if(currentval == "") return;
+            fullVal += currentval + " ^ ";
+            updateMode("^");
+            break;
+
+        default:
+            currentval += e;
+            break;
+      }
+    }
+
+    fullVal += currentval + " = ";
+    updateMode("=");
+    fullVal += sum;
+    currentval = sum;
+
+    return fullVal;
   }
 };
