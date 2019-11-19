479703c2a4ef300ca5998a28d37d88fe7f0fa2c6
Lil Sizzurp <45602120+LilSizzurp@users.noreply.github.com>
2019-11-19 11:33:34 +0100

    Clean up code a tiny bit.

--- index.js
+++ index.js
@@ -6,30 +6,6 @@ module.exports = class Calc extends Plugin {
   }
 
   calc(args) {
-    /*if(args[0] && args[1] && args[2]) {
-      var num1 = Number(args[0]);
-      var op = args[1];
-      var num2 = Number(args[2]);
-
-      switch(op) {
-        case "+":
-          return num1 + num2;
-        case "-":
-          return num1 - num2;
-        case "*":
-          return num1 * num2;
-        case "/":
-          if(num1 == 0 || num2 == 0) return "Cannot divide by zero.";
-          return num1 * num2;
-        case "^":
-          return Math.pow(num1, num2);
-        default:
-          return `Unknown Operator: ${op}`;
-      }
-    } else {
-      return `Usage: ${powercord.api.commands.prefix} calc <num1> <op> <num2>`;
-    }*/
-
     var state = "none";
     var addsub = [];
     var prevVal = 0;
@@ -37,12 +13,6 @@ module.exports = class Calc extends Plugin {
     var fullVal = "";
     var sum = 0;
     var math = args.join(" ");
-      
-    function gatherdata(){
-      console.log("currentval = " + currentval);
-      console.log("prevVal = " + prevVal);
-      console.log("sum = " + sum);
-    }
 
     function updateMode(input){
       if(state != "none") {
